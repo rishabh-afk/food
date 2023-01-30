0fbc6c19b4959c03862b5229d691116d510454e1
Rishabh gupta <guptarishabh792@gmail.com>
2023-01-30 18:28:27 +0530

    make navbar and footer

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
+import { MdMarkEmailRead } from "react-icons/md";
+import { AiFillPhone } from "react-icons/ai";
+import { GiFoodTruck } from "react-icons/gi";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className='h-96 bg-gray-300'>
+    <div className='p-8 grid grid-cols-4 gap-10 '>
+        <div className='flex flex-col gap-5 items-center'>
+          <NavLink to="/">
+            <GiFoodTruck size={100} color="black" />
+          </NavLink>
+          <p>Dear diet, things just aren’t looking good for the both of us. It’s not me, it’s you. You’re too much work. You’re boring and I can’t stop cheating on you.</p>
+        </div>
+        <ul className='flex flex-col gap-4 hover:cursor-pointer text-lg'>
+          <li>Gallery</li>
+          <li>Blogs</li>
+          <li>Contact Us</li>
+          <li>About Us</li>
+          <li>Terms & Condition</li>
+          <li>Privacy Policy</li>
+        </ul>
+        <ul className='flex flex-col gap-4 hover:cursor-pointer text-lg'>
+          <li>Experience With Us</li>
+          <li>Know More</li>
+          <li>Free Delivery 24/7</li>
+          <li>Get free Vouchers</li>
+          <li>Refund & Return</li>
+        </ul>
+        <div className='flex flex-col gap-5 text-lg hover:cursor-pointer'>
+          <p>WZ- 228A, Street Number - 16/3, Sadh Nagar Palam Colony New Delhi, PINCODE - 110045</p>
+          <p className='flex items-center'><MdMarkEmailRead size={25} /> <span>: guptarishabh792@gmail.com</span></p>
+          <p className='flex items-center'><AiFillPhone size={25} /> <span>: +91 9354697528</span></p>
+        </div>
+    </div>
+    <hr />
+    <div className='flex justify-between px-8 py-5 items-center'>
+      <p>All Rights Reserved | @ Copywrite - 2023 </p>
+      <span>V 1.1.1</span>
+    </div>
+    </section>
   )
 }
 
--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
+import { GiFoodTruck } from "react-icons/gi";
+import { AiFillHome } from "react-icons/ai";
+import { GrProductHunt } from "react-icons/gr";
+import { RiGalleryFill } from "react-icons/ri";
+import { MdPermContactCalendar } from "react-icons/md";
+import { FaCartArrowDown, FaBook } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <section className='h-28 flex justify-between p-4 bg-gray-800 shadow-2xl'>
+    <div className='flex gap-14 items-center'>
+      <NavLink to="/">
+        <GiFoodTruck className='ml-4' size={80} color="white" />
+      </NavLink>
+      <div>
+      <ul className='flex gap-10 text-xl font-medium text-white'>
+        <li className='flex flex-col items-center'>
+          <AiFillHome />
+          <span>Home</span>
+        </li>
+        <li className='flex flex-col items-center'>
+          <GrProductHunt />
+          <span>Products</span>
+        </li>
+        <li className='flex flex-col items-center'>
+          <RiGalleryFill />
+          <span>Gallery</span>
+        </li>
+        <li className='flex flex-col items-center'>
+          <MdPermContactCalendar />
+          <span>Contact Us</span>
+        </li>
+      </ul>
+    </div>
+    </div>
+    <div className='flex items-center mr-10'>
+      <ul className='flex gap-10 text-xl font-medium text-white'>
+        <li className='flex gap-2 items-center'>
+          <FaBook size={40} />
+          <div className='flex flex-col'>
+            <span>My</span>
+            <span>Bookings</span>
+          </div>
+        </li>
+        <li className='flex gap-2 items-center'>
+          <FaCartArrowDown size={40} />
+          <div className='flex flex-col'>
+            <span>My</span>
+            <span>Cart</span>
+          </div>
+        </li>
+        <li className='flex gap-2 items-center'>
+        <CgProfile size={40} />
+          <div className='flex flex-col'>
+            <span>My</span>
+            <span>Profile</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+    </section>
   )
 }
 
